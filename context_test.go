64f0245a8f7abd5382ec736ef1057c6e2d82ea0b
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-10 07:28:45 +0800

    context with timeout

--- context_test.go
+++ context_test.go
@@ -84,3 +84,23 @@ func TestContextWithCancel(t *testing.T) {
 
 	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 }
+
+// Context with timeout
+func TestContextWithTimeout(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5 * time.Second)
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
